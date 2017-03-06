ef5742a00e967d162f249df867ceb184dd719615
Eric McFarland <eric.b.mcfarland@gmail.com>
2017-03-05 22:32:41 -0800

    Completed

--- AAS/Exact Change.js
+++ AAS/Exact Change.js
@@ -14,7 +14,7 @@ function checkCashRegister(price, cash, cid) {
   //              Register.change(price, cash) //return the amount of change required and updates the Register
   //              Register.cashIn(price, cash) //Adds the cash quanties to till THIS MAY NOT BE NEEDED FOR EXACT PROBLEM
   //              Register.cashOut()            //
-  //              Transaction. updateQuanties. UpdateTotalValue. isSuffienctFunds. determineChange
+  //              Transaction. updateQuantity. UpdateTotalValue. isSuffienctFunds. determineChange
   //
   //
   var Till = {
@@ -71,20 +71,19 @@ function checkCashRegister(price, cash, cid) {
     },
 
     ////////Methods////////
-
-    "updateQuanties": function () {
-      for (currency in this) {
-        if (this.hasOwnProperty(currency)) {
-          this[currency].quantity = this[currency].totalValue / this[currency].value;
-        }
+    "updateQuantity": function (currency) {
+      if (this[currency].hasOwnProperty("quantity")) {
+        this[currency].quantity = Math.round((this[currency].totalValue / this[currency].value) * 100) / 100;
       }
     },
+
 
     "changeTotalValue": function (currency, totalval) {
 
       if (this.hasOwnProperty(currency)) {
         if ((totalval * 100) % (this[currency].value * 100) == 0) {
           this[currency].totalValue += totalval;
+          this[currency].totalValue = Math.round((this[currency].totalValue) * 100) / 100;
         }
       } else {
         console.log("updatTV error");
@@ -102,61 +101,71 @@ function checkCashRegister(price, cash, cid) {
       }
       return sum;
     },
+
     "dispenseChange": function (price, cash) {
       var change = cash - price;
-      var changeGiven = []
-      ammountInTill =this.totalInTill();
-      if (ammountInTill < change) {
-        return "Insufficient Funds"
-      } else if (ammountInTill == 0) {
-        return "Closed"
-      }
-      console.log("starting change: "+change)
+      var changeGiven = {};
+      console.log("starting change: " + change)
+
       for (currency in this) {
         if (this[currency].hasOwnProperty("value")) {
-          
+
           console.log(currency);
-          while (this[currency].value <= change & this[currency].quantity>0){
-            change = change-this[currency].value;
-            console.log("remaing change: "+change);
+          while (this[currency].value <= change && this[currency].totalValue > 0) {
+            console.log(this[currency])
+            if (changeGiven.hasOwnProperty(currency) == false) {
+              changeGiven[currency] = 0;
+            }
+            change = change - this[currency].value;
+            change = Math.round(change * 100) / 100;
+            console.log("remaing change: " + change);
             this.changeTotalValue(currency, -this[currency].value);
-            this.updateQuanties();
+            this.updateQuantity(currency);
+            console.log(this[currency]);
+            changeGiven[currency] += this[currency].value;
+            changeGiven[currency] = Math.round((changeGiven[currency]) * 100) / 100;
+            console.log("changeGiven")
+            console.log(changeGiven);
           }
         }
-
       }
-      return "testing"
+      //conver changeGiven to an array
 
+      console.log("ammount in till: " + this.totalInTill());
+      if (change!==0) {                     //couldnt find a way to make change
+        return "Insufficient Funds"
+      } else if (this.totalInTill() === 0) {           //All money was given on that transaction
+        return "Closed"
+      } else {
+        var changeArray = Object.keys(changeGiven).map(function (currency) { return [currency, changeGiven[currency]] });
+        console.log("changearray")
+        console.log(changeArray)
+
+        return changeArray
+      }
     },
   }
-    //initialize the till with the CID ammounts
-    cid.forEach(function (val, ind) {
-      Till.changeTotalValue(...val);
-    })
-  Till.updateQuanties();
-
-    var change = cash - price;
-    ammountInTill = Till.totalInTill();
-
-    // Check if register can handle the transaction 
-    if(ammountInTill < change) {
-      return "Insufficient Funds"
-    } else if (ammountInTill == 0) {
-      return "Closed"
-    }
+  //initialize the till with the CID ammounts
+  cid.forEach(function (val, ind) {
+    Till.changeTotalValue(...val);
+    Till.updateQuantity(val[0]);
+  })
+  console.log("Till");
+  console.log(Till);
 
   //Calculates the change that uses the least quantity of change. 
-    console.log(Till.dispenseChange(price,cash));
+  // console.log(Till.dispenseChange(price, cash));
+  return Till.dispenseChange(price, cash)
 
 
 
 
 
-  }
+}
 
-  checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-
-
-
+//checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+// checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+// checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 
 
