197433a1aab17788cee3ea14654e70672eea9a63
Eric McFarland <eric.b.mcfarland@gmail.com>
2017-04-08 13:34:39 -0700

    Most of UI working. Need to game algorithm

--- Emote Wars/script.js
+++ Emote Wars/script.js
@@ -1,6 +1,53 @@
+//approach to grabbing all emotes:
+//  Get emoticon ids from JSONs
+//  Use that id to fill this https://static-cdn.jtvnw.net/emoticons/v1/ID/1.0
+
+// TODO: 
+//  Start on PredPrey algorithm
+var board = [], selectedEmote = 123440;
+board.drawBoard = function () {
+    //This function updates the displayBoard HTML elements with the current state of the board array
+    console.log("board is: ")
+    console.log(this);
+    var displayTable = document.getElementById("board");
+
+    //loop each space 
+    var displayRow = displayTable.firstChild;
+    var displayCol = displayTable.firstChild.firstChild;
+    this.forEach(function (row, rowIndex, rowArr) {
+        row.forEach(function (col, colIndex, colArr) {
+            //based on creatures in the board, update the html elements
+            if (col instanceof Creature) {
+                displayCol.style.backgroundImage = col.getImage();
+            } else {
+                displayCol.style.backgroundImage = '';
+            }
+
+            if (colIndex < colArr.length - 1) {
+                displayCol = displayCol.nextSibling;
+            }
+        })
+        if (rowIndex < rowArr.length - 1) {
+            displayRow = displayRow.nextSibling;
+            displayCol = displayRow.firstChild;
+        }
+    })
+}
+board.move = function (oldX, oldY, newX, newY) {
+    this[newX][newY] = this[oldX][oldY];
+    this[oldX][oldY] = [];
+}
+board.moveUp = function (oldX, oldY) {
+    if (oldY > 0) {
+        this[oldY - 1][oldX] = this[oldY][oldX];
+        this[oldY][oldX] = [];
+    }
+}
 $(document).ready(function () {
-    makeTable(5, 5);
+
+    makeBoard(5, 5);
     getEmotes();
+    board.drawBoard();
     //Want to swap this to pure JS
     // $(".counter").on("click", function () {
 
@@ -17,40 +64,48 @@ $(document).ready(function () {
     //             break
     //     }
     // })
+
 })
+function gameLoop() {
+    // makeCreatureList();
+    board.moveUp(1, 1);
+    board.drawBoard();
 
-function makeTable(height, width) {
-
-    var rows, cols, tab, tr, td, test, boardHeight = height, boardWidth = width;
-
-    test = document.createElement("td");
+}
+function makeBoard(height, width) {
+    var rows, cols, tab, tr, td, boardHeight = height, boardWidth = width;
     tab = document.getElementById("board");
+
     //clear existing board
     while (tab.firstChild) {
         tab.removeChild(tab.firstChild);
+        board.pop();
     }
 
-    //create the board spaces
+    //create the board and displayBoard spaces
     for (var row = 0; row < boardHeight; row++) {
         tr = document.createElement('tr');
+        board.push([]);
 
         for (var col = 0; col < boardWidth; col++) {
+            board[row].push([]);
             td = document.createElement("td");
             td.className += "boardSpace";
-            td.onclick = function () {      //Make this a callable function with img as argument
+            td.onclick = function () {
                 var square = event.target;
-                console.log(square.firstChild);
-                if (square.firstChild) {
-                    console.log("remove img")
-                    square.removeChild(square.firstChild);
-                } else {
-                    var image = document.createElement("img");
-                    image.setAttribute('src', 'https://static-cdn.jtvnw.net/emoticons/v1/123440/1.0');
-                    square.appendChild(image);
-                    // if (square.className == "boardSpace") {
-                    //     square.className += " greenBackground";
-                    // }
+                var posY = square.parentElement.rowIndex;
+                var posX = square.cellIndex;
+
+                if (board[posY][posX] instanceof Creature && board[posY][posX].emoteId === selectedEmote) {//if square is already a creature, replace if new emote. remove if same
+                    board[posY][posX] = [];
                 }
+                else {                                                          //if not create creature at that board space, and set image
+                    creature = new Creature(posX, posY, selectedEmote);
+                    board[posY][posX] = creature;
+
+
+                }
+                board.drawBoard();
             }
             tr.appendChild(td);
         }
@@ -59,17 +114,72 @@ function makeTable(height, width) {
     document.getElementById("board").rows[2].cells[2].className += " redBackground";
 }
 
-//getting the full list of images is waaaay too slow
+function Creature(posX, posY, emoteId) {
+    this.posX = posX;
+    this.posY = posY;
+    this.emoteId = emoteId;
+    this.moveSpeed = 1;
+    this.getImage = function () { return "url('https://static-cdn.jtvnw.net/emoticons/v1/" + this.emoteId + "/1.0')" };
+    this.moveUp = function () {
+        if (this.posX > 0) {
+            this.posY = this.posY - 1 * this.moveSpeed;
+        }
+    };
+}
+
+//getting the full list of images is waaaay too slow 
 function getEmotes() {
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET', 'https://twitchemotes.com/api_cache/v2/sets.json', true);
+    xmlhttp.open('GET', 'https://twitchemotes.com/api_cache/v2/subscriber.json', true);
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4) {
             if (xmlhttp.status == 200) {
                 var obj = JSON.parse(xmlhttp.responseText);
-                console.log(obj)
+                console.log(obj);
+                //NEED CHECK FOR if channels + person + emotes exists 
+                emotes = obj.channels.moonmoon_ow.emotes
+                emoteId = obj.channels.moonmoon_ow.emotes[0].image_id;
+                console.log('emoteId: ' + emoteId);
+                tab = document.getElementById("emotes");
+
+                for (emote in emotes) {
+                    emoteId = emotes[emote].image_id;
+                    emoteName = emotes[emote].code;
+
+                    emoteImg = document.createElement("img");
+                    emoteImg.className = "emote";
+                    //can add an ID
+
+                    emoteImg.setAttribute("src", 'https://static-cdn.jtvnw.net/emoticons/v1/' + emoteId + '/1.0');
+                    emoteImg.setAttribute("title", emoteName);
+                    emoteImg.setAttribute("name", emoteId);
+                    emoteImg.onclick = function () {
+                        //create a new Creature and update Array then draw board
+                        targetEmote = event.target;
+                        selectedEmote = targetEmote.getAttribute("name");
+                        allImgs = document.getElementsByClassName("emote");
+                        console.log(document.getElementsByClassName("emote")); 
+                        console.log(allImgs[0].className) 
+                        
+                        
+                        // = document.getElementsByClassName("emote").className.replace
+                        //     (/(?:^|\s)emoteBorder(?!\S)/g, '')
+                        for (var index = 0; index < allImgs.length; index++) {
+                            var element = allImgs[index];
+                            element.className = element.className.replace
+                            (/(?:^|\s)emoteBorder(?!\S)/g, '')
+                            
+                        }
+                        targetEmote.className += " emoteBorder";
+                    }
+                    tab.appendChild(emoteImg);
+                }
+
+
             }
+
         }
-    };
+    }
+
     xmlhttp.send(null);
 }
